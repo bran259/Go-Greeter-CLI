f3449ab279659e4ae93f50c1afdc3a8b4b54a802
bran259 <kiprop686@gmail.com>
2025-11-19 20:33:11 +0300

    main.go sample practice

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strings"
+)
+
+func main() {
+    reader := bufio.NewReader(os.Stdin)
+
+    fmt.Print("What is your name? ")
+
+    name, _ := reader.ReadString('\n')
+    name = strings.TrimSpace(name)
+
+    fmt.Printf("Hello, %s! Welcome to the Go Greeter CLI.\n", name)
+}
